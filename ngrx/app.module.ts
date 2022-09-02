977f20728c661f60af195c54b476c46177845ff8
gsoulie <gsoulie@users.noreply.github.com>
2022-09-02 16:30:12 +0200

    Create app.module.ts

---
+++ ngrx/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { usersReducer } from './components/users/ngrx-store/users.reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot({ userState: usersReducer}) // <-- NgRx
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
