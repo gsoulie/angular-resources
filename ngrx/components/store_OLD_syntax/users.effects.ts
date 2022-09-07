adef910ae9963edcea917086c45924aa1f575760
gsoulie <gsoulie@users.noreply.github.com>
2022-09-07 16:28:29 +0200

    Create users.effects.ts

---
+++ ngrx/components/store_OLD_syntax/users.effects.ts
@@ -0,0 +1,34 @@
+import { User } from '../user.model';
+import { HttpClient } from '@angular/common/http';
+import { Actions, ofType, createEffect } from "@ngrx/effects";
+import * as UserActions from './users.actions';
+import { map } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserEffects {
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient) { }
+
+  fetchData$ = createEffect((): any => {
+    return this.actions$.pipe(
+      ofType(UserActions.FETCH_USERS),  // ofType permet de filtrer sur le type d'action à trigger. Il est possible ici de spécifier plusieurs actions
+      switchMap(() => this.getUsers() ),
+      map(users => {
+        return users.map(user => {
+          return {
+            ...user
+          }
+        })
+      }),
+      map(users => new UserActions.SetUsers(users))
+    )
+  });
+
+  getUsers() { return this.http.get<User[]>('/assets/data.json'); }
+
+
+}
