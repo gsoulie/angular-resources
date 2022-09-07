bedf96988de05b87c9e2db554266ab18d579f174
gsoulie <gsoulie@users.noreply.github.com>
2022-09-07 16:29:05 +0200

    Update users.actions.ts

--- ngrx/components/store/users.actions.ts
+++ ngrx/components/store/users.actions.ts
@@ -1,38 +1,14 @@
-import { Action } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 import { User } from "../user.model";
 
-export const ADD_USER = '[User] Add user';
-export const UPDATE_USER = '[User] Update user';
-export const DELETE_USER = '[User] Delete user';
-export const FETCH_USERS = '[User] Fetch users';
-export const SET_USERS = '[User] Set users';
+const ADD_USER = '[User] Add user';
+const UPDATE_USER = '[User] Update user';
+const DELETE_USER = '[User] Delete user';
+const FETCH_USERS = '[User] Fetch users';
+const SET_USERS = '[User] Set users';
 
-export class FetchUsers implements Action {
-  readonly type = FETCH_USERS;
-}
-export class SetUsers implements Action {
-  readonly type = SET_USERS;
-
-  constructor(public payload: User[]) {}
-}
-
-export class AddUser implements Action {
-  readonly type = ADD_USER;
-
-  constructor(public payload: User) { }
-}
-
-export class DeleteUser implements Action {
-  readonly type = DELETE_USER;
-
-  constructor(public payload: User) { }
-}
-
-export class UpdateUser implements Action {
-  readonly type = UPDATE_USER;
-
-  constructor(public payload: User) { }
-}
-
-// exporter un nouveau type contenant les différents type d'actions
-export type UserActions = FetchUsers | SetUsers | AddUser | DeleteUser | UpdateUser;
+export const fetchUsers = createAction(FETCH_USERS);
+export const setUsers = createAction(SET_USERS, props<{ payload: User[] }>())
+export const addUser = createAction(ADD_USER, props<{ payload: User }>());
+export const deleteUser = createAction(DELETE_USER, props<{ payload: User }>());
+export const updateUser = createAction(UPDATE_USER, props<{ payload: User }>());
