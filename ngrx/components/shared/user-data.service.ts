23919c6cd629963d2077f461d82c93b069fb0684
gsoulie <gsoulie@users.noreply.github.com>
2022-09-07 16:36:25 +0200

    Update user-data.service.ts

--- ngrx/components/shared/user-data.service.ts
+++ ngrx/components/shared/user-data.service.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { User } from './user.model';
 import * as fromApp from '../../shared/store/app.reducer'
 import { Store } from '@ngrx/store';
 import * as UserActions from './ngrx-store/users.actions';
@@ -11,46 +8,12 @@ import * as UserActions from './ngrx-store/users.actions';
 })
 export class UserDataService {
 
-  private usersSubject$ = new BehaviorSubject<User[]>([]);
-  users$: Observable<User[]> = this.usersSubject$.asObservable();
+  constructor(private store: Store<fromApp.AppGlobalState>) { }
 
-  constructor(private http: HttpClient, private store: Store<fromApp.AppGlobalState>, private actions$: Actions) { }
-
-  fetchUsers(): Observable<User[]> {
-    // Ancienne syntaxe
-    /*return this.http.get<User[]>('/assets/data.json')
-      .pipe(
-        tap({
-          res => this.usersSubject$.next(res)
-          this.store.dispatch(new UserActions.InitUsers(res));
-        })
-      );*/
-    this.store.dispatch(new UserActions.FetchUsers());
-    return this.actions$.pipe(
-      ofType(UserActions.SET_USERS),
-      take(1)
-    );
-  }
-
-  addUser(user: User) {
-    const users = this.usersSubject$.value;
-    users.push(user);
-    this.usersSubject$.next(users);
-  }
-
-  deleteUser(id) {
-    const users = this.usersSubject$.value;
-    const index = users.findIndex(p => p.id === id);
-
-    if (index >= 0) {
-      users.splice(index, 1);
-      this.usersSubject$.next(users);
-    }
-  }
-
-  updateUser(user: User) {
-    let users = this.usersSubject$.value;
-    users = users.map(u => u.id !== user.id ? u : user);
-    this.usersSubject$.next(users);
+  fetchUsers() {
+    this.store.dispatch(UserActions.fetchUsers());
+    
+    // Ancienne syntaxe ci-dessous -->
+    //this.store.dispatch(new UserActions.FetchUsers());
   }
 }
