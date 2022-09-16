9fe0afd2be214a396574699b828c5b3d798aacdf
gsoulie <gsoulie@users.noreply.github.com>
2022-09-16 15:22:28 +0200

    Update user-data.service.ts

--- ngrx/components/shared/user-data.service.ts
+++ ngrx/components/shared/user-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as fromApp from '../../shared/store/app.reducer'
 import { Store } from '@ngrx/store';
-import * as UserActions from './ngrx-store/users.actions';
+import { UsersActions } from "./ngrx-store/users.actions";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class UserDataService {
   constructor(private store: Store<fromApp.AppGlobalState>) { }
 
   fetchUsers() {
-    this.store.dispatch(UserActions.fetchUsers());
+    this.store.dispatch(UsersActions.fetch_users());
     
     // Ancienne syntaxe ci-dessous -->
     //this.store.dispatch(new UserActions.FetchUsers());
