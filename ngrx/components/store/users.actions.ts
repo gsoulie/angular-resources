8f3a681005508da5b86094225602376156b67ba1
gsoulie <gsoulie@users.noreply.github.com>
2022-09-16 15:15:54 +0200

    Update users.actions.ts

--- ngrx/components/store/users.actions.ts
+++ ngrx/components/store/users.actions.ts
@@ -7,8 +7,21 @@ const DELETE_USER = '[User] Delete user';
 const FETCH_USERS = '[User] Fetch users';
 const SET_USERS = '[User] Set users';
 
-export const fetchUsers = createAction(FETCH_USERS);
+// Déclaration d'un groupe d'action - Nouvelle syntaxe NgRx 14
+export const UsersActions = createActionGroup({
+  source: 'Users',
+  events: {
+    FETCH_USERS: emptyProps(),
+    SET_USERS: props<{ payload: User[] }>(),
+    ADD_USER: props<{ payload: User }>(),
+    DELETE_USER: props<{ payload: User }>(),
+    UPDATE_USER: props<{ payload: User }>()
+  }
+})
+
+// Déclaration des action NgRx < 14
+/*export const fetchUsers = createAction(FETCH_USERS);
 export const setUsers = createAction(SET_USERS, props<{ payload: User[] }>())
 export const addUser = createAction(ADD_USER, props<{ payload: User }>());
 export const deleteUser = createAction(DELETE_USER, props<{ payload: User }>());
-export const updateUser = createAction(UPDATE_USER, props<{ payload: User }>());
+export const updateUser = createAction(UPDATE_USER, props<{ payload: User }>());*/
