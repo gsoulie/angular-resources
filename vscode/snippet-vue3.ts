650bfa86b590b943e0f8b90083bb03995a1d4dc2
gsoulie <gsoulie@users.noreply.github.com>
2022-11-03 10:18:58 +0100

    Create snippet-vue3.ts

---
+++ vscode/snippet-vue3.ts
@@ -0,0 +1,29 @@
+{
+	"Snippet structure vide": {
+		"prefix": "vue",
+		"body": [
+			"<template>",
+			"\t<div>",
+			"\t</div>",
+			"</template>\r\r",
+			"<script lang=\"ts\">",
+			"export default {",
+			"\tname: '$1',",
+			"\tcomponents: {",
+			"\t},",
+			"\tsetup() {",
+			"\t\treturn {}",
+			"\t}",
+			"}",
+			"</script>",
+			"<style lang=\"scss\">\r\r",
+			"</style>"
+		]
+	},
+	"v-for": {
+		"prefix": "v-for",
+		"body": [
+			"v-for=\"(item, index) in users\" :key=\"index\""
+		]
+	}
+}
