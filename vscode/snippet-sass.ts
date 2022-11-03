5a90841c30e5e3ada80592c1cadc66e00208dc0b
gsoulie <gsoulie@users.noreply.github.com>
2022-11-03 10:19:31 +0100

    Create snippet-sass.ts

---
+++ vscode/snippet-sass.ts
@@ -0,0 +1,12 @@
+{
+	"media queries": {
+		"prefix": "media",
+		"body": [
+			"@media screen and (max-width: 480px){}",
+			"@media screen and (min-width: 768px){}",
+			"@media screen and (min-width: 1024px){}",
+			"@media screen and (max-width: 1279px){}",
+			"@media screen and (min-width: 1280px){}",
+		]
+	}
+}
