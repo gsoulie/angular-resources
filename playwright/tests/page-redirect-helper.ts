9c8fafab0a83ec0b5f2b882d8885eefd3d70e1b0
gsoulie <gsoulie@users.noreply.github.com>
2025-03-25 16:29:48 +0100

    Update page-redirect-helper.ts

--- playwright/tests/page-redirect-helper.ts
+++ playwright/tests/page-redirect-helper.ts
@@ -1,1 +1,23 @@
 
+import { expect, Page } from "@playwright/test";
+
+/**
+ * Aller sur une page spécifique
+ * @param page 
+ * @param url : url à atteindre
+ */
+export const redirectTo = async (page: Page, url: string) => {
+  await page.goto(url);
+  await page.waitForURL(url);
+  await expect(page).toHaveURL(url);
+}
+
+/**
+ * Contrôler l'url passée en paramètre
+ * @param page 
+ * @param url 
+ */
+export const checkIfUrlIsLoaded = async (page: Page, url: string) => {
+  await page.waitForURL(url);
+  await expect(page).toHaveURL(url);
+}
