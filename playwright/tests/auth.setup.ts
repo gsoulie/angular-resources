53b05d325a790e66ae6f5508725a9a77b64230f4
gsoulie <gsoulie@users.noreply.github.com>
2025-03-26 15:27:58 +0100

    Update auth.setup.ts

--- playwright/tests/auth.setup.ts
+++ playwright/tests/auth.setup.ts
@@ -9,15 +9,18 @@ setup('Authentification', async ({ page }) => {
 
   const emailField = await page.locator('#email');
   await expect(emailField).toBeVisible();
-  await page.locator('#email').fill('xxx@xxx.com');
+  await page.locator('#email').fill(process.env.AUTH_EMAIL!); // process.env.AUTH_EMAIL! est utilisé par la pipeline CI/CD. 
+                                                              //Pour des tests exécutés depuis vscode, il faut renseigner la valeur du login/pwd en clair ici
 
   const passwordField = await page.locator('#password');
   await expect(passwordField).toBeVisible();
-  await passwordField.fill('xxx');
+  await passwordField.fill('xxx'); // process.env.AUTH_PASSWORD! est utilisé par la pipeline CI/CD. 
+                                   //Pour des tests exécutés depuis vscode, il faut renseigner la valeur du login/pwd en clair ici
 
   const loginButton = page.getByRole('button', { name: 'Se connecter' });
   await expect(loginButton).toBeVisible();
   await loginButton.click();
+  await page.waitForTimeout(2000); // Vérifie si la page change
 
   // Attendre la redirection ou vérifier que l'utilisateur est connecté
   await page.waitForURL('https://my-app/dashboard');
